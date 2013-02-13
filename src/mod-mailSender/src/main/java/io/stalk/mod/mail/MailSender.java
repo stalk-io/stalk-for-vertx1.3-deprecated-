10d2c8091c3589d0bbb2af3699ea36ea3c1ab08e
John Kim <stalk.io.developer@gmail.com>
2013-02-13 23:01:37 +0900

    create modules

--- src/mod-mailSender/src/main/java/io/stalk/mod/mail/MailSender.java
+++ src/mod-mailSender/src/main/java/io/stalk/mod/mail/MailSender.java
@@ -1,10 +1,7 @@
 package io.stalk.mod.mail;
 
-import org.vertx.java.busmods.BusModBase;
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.eventbus.Message;
-import org.vertx.java.core.json.JsonArray;
-import org.vertx.java.core.json.JsonObject;
+import java.util.Date;
+import java.util.Properties;
 
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -13,162 +10,169 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.util.Date;
-import java.util.Properties;
+
+import org.vertx.java.busmods.BusModBase;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
 
 public class MailSender extends BusModBase implements Handler<Message<JsonObject>> {
 
-  private Session session;
-  private Transport transport;
+	private Session 	session;
+	private Transport 	transport;
 
-  private String address;
-  private boolean ssl;
-  private String host;
-  private int port;
-  private boolean auth;
-  private String username;
-  private String password;
-  private String contentType;
+	private String 		address;
+	private boolean 	ssl;
+	private String 		host;
+	private int 		port;
+	private boolean 	auth;
+	private String 		username;
+	private String 		password;
+	private String 		contentType;
 
-  @Override
-  public void start() {
-    super.start();
-    address = getOptionalStringConfig("address", "vertx.mailer");
-    ssl = getOptionalBooleanConfig("ssl", false);
-    host = getOptionalStringConfig("host", "localhost");
-    port = getOptionalIntConfig("port", 25);
-    auth = getOptionalBooleanConfig("auth", false);
-    username = getOptionalStringConfig("username", null);
-    password = getOptionalStringConfig("password", null);
-    contentType = getOptionalStringConfig("content_type", "text/plain");
+	@Override
+	public void start() {
+		super.start();
+		address 	= getOptionalStringConfig("address", "vertx.mailer");
+		ssl 		= getOptionalBooleanConfig("ssl", false);
+		host 		= getOptionalStringConfig("host", "localhost");
+		port 		= getOptionalIntConfig("port", 25);
+		auth 		= getOptionalBooleanConfig("auth", false);
+		username 	= getOptionalStringConfig("username", null);
+		password 	= getOptionalStringConfig("password", null);
+		contentType = getOptionalStringConfig("content_type", "text/plain");
 
-    eb.registerHandler(address, this);
+		eb.registerHandler(address, this);
 
-    Properties props = new Properties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.host", host);
-    props.put("mail.smtp.socketFactory.port", Integer.toString(port));
-    if (ssl) {
-      props.put("mail.smtp.socketFactory.class",
-        "javax.net.ssl.SSLSocketFactory");
-    }
-    props.put("mail.smtp.socketFactory.fallback", Boolean.toString(false));
-    props.put("mail.smtp.auth", Boolean.toString(auth));
-    //props.put("mail.smtp.quitwait", "false");
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.socketFactory.port", Integer.toString(port));
+		if (ssl) {
+			props.put("mail.smtp.socketFactory.class",
+					"javax.net.ssl.SSLSocketFactory");
+		}
+		props.put("mail.smtp.socketFactory.fallback", Boolean.toString(false));
+		props.put("mail.smtp.auth", Boolean.toString(auth));
+		//props.put("mail.smtp.quitwait", "false");
 
-    session = Session.getInstance(props,
-        new javax.mail.Authenticator() {
-          protected PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication(username, password);
-          }
-        });
-    //session.setDebug(true);
+		session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		//session.setDebug(true);
 
-    try {
-      transport = session.getTransport();
-      transport.connect();
-    } catch (MessagingException e) {
-      logger.error("Failed to setup mail transport", e);
-    }
-  }
+		try {
+			transport = session.getTransport();
+			transport.connect();
+		} catch (MessagingException e) {
+			logger.error("Failed to setup mail transport", e);
+		}
+	}
 
-  @Override
-  public void stop() {
-    try {
-      transport.close();
-    } catch (MessagingException e) {
-      logger.error("Failed to stop mail transport", e);
-    }
-  }
+	@Override
+	public void stop() {
+		try {
+			transport.close();
+		} catch (MessagingException e) {
+			logger.error("Failed to stop mail transport", e);
+		}
+	}
 
-  private InternetAddress[] parseAddresses(Message<JsonObject> message, String fieldName,
-                                           boolean required)
-  {
-    Object oto = message.body.getField(fieldName);
-    if (oto == null) {
-      if (required) {
-        sendError(message, fieldName + " address(es) must be specified");
-      }
-      return null;
-    }
-    try {
-      InternetAddress[] addresses = null;
-      if (oto instanceof String) {
-        addresses = InternetAddress.parse((String)oto, true);
-      } else if (oto instanceof JsonArray) {
-        JsonArray loto = (JsonArray)oto;
-        addresses = new InternetAddress[loto.size()];
-        int count = 0;
-        for (Object addr: loto) {
-          if (addr instanceof String == false) {
-            sendError(message, "Invalid " + fieldName + " field");
-            return null;
-          }
-          InternetAddress[] ia = InternetAddress.parse((String)addr, true);
-          addresses[count++] = ia[0];
-        }
-      }
-      return addresses;
-    } catch (AddressException e) {
-      sendError(message, "Invalid " + fieldName + " field");
-      return null;
-    }
-  }
+	private InternetAddress[] parseAddresses(Message<JsonObject> message, String fieldName,
+			boolean required)
+	{
+		Object oto = message.body.getField(fieldName);
+		if (oto == null) {
+			if (required) {
+				sendError(message, fieldName + " address(es) must be specified");
+			}
+			return null;
+		}
+		try {
+			InternetAddress[] addresses = null;
+			if (oto instanceof String) {
+				addresses = InternetAddress.parse((String)oto, true);
+			} else if (oto instanceof JsonArray) {
+				JsonArray loto = (JsonArray)oto;
+				addresses = new InternetAddress[loto.size()];
+				int count = 0;
+				for (Object addr: loto) {
+					if (addr instanceof String == false) {
+						sendError(message, "Invalid " + fieldName + " field");
+						return null;
+					}
+					InternetAddress[] ia = InternetAddress.parse((String)addr, true);
+					addresses[count++] = ia[0];
+				}
+			}
+			return addresses;
+		} catch (AddressException e) {
+			sendError(message, "Invalid " + fieldName + " field");
+			return null;
+		}
+	}
 
-  public void handle(Message<JsonObject> message) {
+	@Override
+	public void handle(Message<JsonObject> message) {
 
-    String from = message.body.getString("from");
+		String from = message.body.getString("from");
 
-    if (from == null) {
-      sendError(message, "from address must be specified");
-      return;
-    }
+		if (from == null) {
+			sendError(message, "from address must be specified");
+			return;
+		}
 
-    InternetAddress fromAddress;
-    try {
-      fromAddress = new InternetAddress(from, true);
-    } catch (AddressException e) {
-      sendError(message, "Invalid from address: " + from, e);
-      return;
-    }
+		InternetAddress fromAddress;
+		try {
+			fromAddress = new InternetAddress(from, true);
+		} catch (AddressException e) {
+			sendError(message, "Invalid from address: " + from, e);
+			return;
+		}
 
-    InternetAddress[] recipients = parseAddresses(message, "to", true);
-    if (recipients == null) {
-      return;
-    }
-    InternetAddress[] cc = parseAddresses(message, "cc", false);
-    InternetAddress[] bcc = parseAddresses(message, "bcc", false);
+		InternetAddress[] recipients = parseAddresses(message, "to", true);
+		if (recipients == null) {
+			return;
+		}
+		InternetAddress[] cc = parseAddresses(message, "cc", false);
+		InternetAddress[] bcc = parseAddresses(message, "bcc", false);
 
-    String subject = message.body.getString("subject");
-    if (subject == null) {
-      sendError(message, "subject must be specified");
-      return;
-    }
-    String body = message.body.getString("body");
-    if (body == null) {
-      sendError(message, "body must be specified");
-      return;
-    }
+		String subject = message.body.getString("subject");
+		if (subject == null) {
+			sendError(message, "subject must be specified");
+			return;
+		}
+		String body = message.body.getString("body");
+		if (body == null) {
+			sendError(message, "body must be specified");
+			return;
+		}
 
-    javax.mail.Message msg = new MimeMessage(session);
+		javax.mail.Message msg = new MimeMessage(session);
 
-    try {
-      msg.setFrom(fromAddress);
-      msg.setRecipients(javax.mail.Message.RecipientType.TO, recipients);
-      msg.setRecipients(javax.mail.Message.RecipientType.CC, cc);
-      msg.setRecipients(javax.mail.Message.RecipientType.BCC, bcc);
-      msg.setSubject(subject);
-      msg.setContent(body, contentType);
-      msg.setSentDate(new Date());
-      transport.send(msg);
-      sendOK(message);
-    } catch (MessagingException e) {
+		try {
+			msg.setFrom(fromAddress);
+			msg.setRecipients(javax.mail.Message.RecipientType.TO, recipients);
+			msg.setRecipients(javax.mail.Message.RecipientType.CC, cc);
+			msg.setRecipients(javax.mail.Message.RecipientType.BCC, bcc);
+			msg.setSubject(subject);
+			msg.setContent(body, contentType);
+			msg.setSentDate(new Date());
+			transport.send(msg);
 
-      sendError(message, "Failed to send message", e);
-    } catch (Throwable t) {
-      t.printStackTrace();
-    }
-  }
+			sendOK(message);
+
+		} catch (MessagingException e) {
+
+			sendError(message, "Failed to send message", e);
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
 
 }
 
--- src/mod-mongoManager/src/main/java/io/stalk/mod/mongo/MongoManager.java
+++ src/mod-mongoManager/src/main/java/io/stalk/mod/mongo/MongoManager.java
@@ -1,100 +1,199 @@
 package io.stalk.mod.mongo;
 
-import java.net.UnknownHostException;
+import io.stalk.common.utils.BijectiveUtils;
 
-import io.stalk.common.api.PUBLISH_MANAGER;
-import io.stalk.common.server.NodeManager;
-import io.stalk.common.server.RedisNodeManager;
-import io.stalk.common.server.node.RedisPoolNode;
+import java.net.UnknownHostException;
+import java.util.UUID;
 
 import org.vertx.java.busmods.BusModBase;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
-import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
-import org.vertx.java.core.logging.Logger;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
+import com.mongodb.WriteConcern;
+import com.mongodb.WriteResult;
+import com.mongodb.util.JSON;
 
 public class MongoManager extends BusModBase implements Handler<Message<JsonObject>> {
 
-	  private String address;
-	  private String host;
-	  private int port;
-	  private String dbName;
-	  private String username;
-	  private String password;
+	private String address;
+	private String host;
+	private int port;
+	private String dbName;
+	private String username;
+	private String password;
+	private String collection;
 
-	  private Mongo mongo;
-	  private DB db;
+	private Mongo mongo;
+	private DB db;
+	private DBCollection dbCollection;
 
-	  public void start() {
-	    super.start();
+	@Override
+	public void start() {
+		super.start();
 
-	    address = getOptionalStringConfig("address", "vertx.mongopersistor");
-	    host = getOptionalStringConfig("host", "localhost");
-	    port = getOptionalIntConfig("port", 27017);
-	    dbName = getOptionalStringConfig("db_name", "default_db");
-	    username = getOptionalStringConfig("username", null);
-	    password = getOptionalStringConfig("password", null);
+		address = getOptionalStringConfig("address", "vertx.mongopersistor");
+		host = getOptionalStringConfig("host", "localhost");
+		port = getOptionalIntConfig("port", 27017);
+		dbName = getOptionalStringConfig("db_name", "default_db");
+		username = getOptionalStringConfig("username", null);
+		password = getOptionalStringConfig("password", null);
+		collection = getOptionalStringConfig("collection", "users");
 
-	    try {
-	      mongo = new Mongo(host, port);
-	      db = mongo.getDB(dbName);
-	      if (username != null && password != null) {
-	        db.authenticate(username, password.toCharArray());
-	      }
-	      eb.registerHandler(address, this);
-	    } catch (UnknownHostException e) {
-	      logger.error("Failed to connect to mongo server", e);
-	    }
-	  }
+		try {
+			mongo = new Mongo(host, port);
+			db = mongo.getDB(dbName);
+			if (username != null && password != null) {
+				db.authenticate(username, password.toCharArray());
+			}
+			dbCollection = db.getCollection(collection);
 
-	  public void stop() {
-	    mongo.close();
-	  }
+			eb.registerHandler(address, this);
+		} catch (UnknownHostException e) {
+			logger.error("Failed to connect to mongo server", e);
+		}
+	}
 
-	  public void handle(Message<JsonObject> message) {
+	@Override
+	public void stop() {
+		mongo.close();
+	}
 
-	    String action = message.body.getString("action");
+	@Override
+	public void handle(Message<JsonObject> message) {
 
-	    if (action == null) {
-	      sendError(message, "action must be specified");
-	      return;
-	    }
+		String action = message.body.getString("action");
 
-	    switch (action) {
-	      case "save":
-	        doSave(message);
-	        break;
-	      case "update":
-	        doUpdate(message);
-	        break;    
-	      case "find":
-	        doFind(message);
-	        break;
-	      case "findone":
-	        doFindOne(message);
-	        break;
-	      case "delete":
-	        doDelete(message);
-	        break;
-	      case "count":
-	        doCount(message);
-	        break;
-	      case "getCollections":
-	        getCollections(message);
-	        break;
-	      case "collectionStats":
-	        getCollectionStats(message);
-	        break;
-	      case "command":
-	        runCommand(message);
-	      default:
-	        sendError(message, "Invalid action: " + action);
-	        return;
-	    }
-	  }
+		if (action == null) {
+			sendError(message, "action must be specified");
+			return;
+		}
 
+		switch (action) {
+		case "save":
+			doSave(message);
+			break;
+		case "update":
+			doUpdate(message);
+			break;    
+		case "find":
+			doFind(message);
+			break;
+		case "findone":
+			doFindOne(message);
+			break;
+		case "delete":
+			doDelete(message);
+			break;
+		case "count":
+			doCount(message);
+			break;
+		case "getCollections":
+			getCollections(message);
+			break;
+		case "collectionStats":
+			getCollectionStats(message);
+			break;
+		case "command":
+			runCommand(message);
+		default:
+			sendError(message, "Invalid action: " + action);
+			return;
+		}
+	}
+
+	private void createUser(Message<JsonObject> message) {
+
+		int seq = getSeq("user_email");
+
+		String key = BijectiveUtils.encode(seq);
+
+		WriteConcern writeConcern = WriteConcern.valueOf(getOptionalStringConfig("writeConcern",""));
+		if (writeConcern == null) writeConcern = db.getWriteConcern();
+		
+		JsonObject doc = getMandatoryObject("document", message);
+		
+		DBObject obj = jsonToDBObject(doc);
+		WriteResult res = dbCollection.save(obj, writeConcern);
+		if (res.getError() == null) {
+			JsonObject reply = new JsonObject();
+			reply.putString("key", key);
+			sendOK(message, reply);
+		} else {
+			sendError(message, res.getError());
+		}
+
+	}
+
+	private void updateUser(Message<JsonObject> message) {
+		
+		
+		
+	}
+
+	private void doSave(Message<JsonObject> message) {
+		String collection = getMandatoryString("collection", message);
+		if (collection == null) {
+			return;
+		}
+		JsonObject doc = getMandatoryObject("document", message);
+		if (doc == null) {
+			return;
+		}
+		String genID;
+		if (doc.getField("_id") == null) {
+			genID = UUID.randomUUID().toString();
+			doc.putString("_id", genID);
+		} else {
+			genID = null;
+		}
+		DBCollection coll = db.getCollection(collection);
+		DBObject obj = jsonToDBObject(doc);
+		WriteConcern writeConcern = WriteConcern.valueOf(getOptionalStringConfig("writeConcern",""));
+		if (writeConcern == null) {
+			writeConcern = db.getWriteConcern();
+		}
+		WriteResult res = coll.save(obj, writeConcern);
+		if (res.getError() == null) {
+			if (genID != null) {
+				JsonObject reply = new JsonObject();
+				reply.putString("_id", genID);
+				sendOK(message, reply);
+			} else {
+				sendOK(message);
+			}
+		} else {
+			sendError(message, res.getError());
+		}
+	}
+
+
+
+
+	private int getSeq(String seqName) {
+		String sequence_collection = "seqs"; 
+		String sequence_field = "seq";
+
+		DBCollection seq = db.getCollection(sequence_collection);
+
+		DBObject query = new BasicDBObject();
+		query.put("_id", seqName);
+
+		DBObject change = new BasicDBObject(sequence_field, 1);
+		DBObject update = new BasicDBObject("$inc", change);
+
+		DBObject res = seq.findAndModify(query, new BasicDBObject(), new BasicDBObject(), false, update, true, true);
+		return Integer.parseInt(res.get(sequence_field).toString());
+	}
+	
+	private DBObject jsonToDBObject(JsonObject object) {
+		String str = object.encode();
+		return (DBObject)JSON.parse(str);
+	}
+	
 }
